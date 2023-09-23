53272308d5d336e98b2c016e3f5d761ce59e3082
Nikhil Reddy <48628715+NikhilChandraReddy@users.noreply.github.com>
2023-09-23 11:47:17 -0400

    Refactor with DRY principle

--- script.js
+++ script.js
@@ -4,33 +4,34 @@ let secretNumber = Math.ceil(Math.random() * 20);
 let currentScore = Number(document.querySelector('.score').textContent);
 let highScore = Number(document.querySelector('.highscore').textContent);
 
+const displayMessage = function (message) {
+  document.querySelector('.message').textContent = message;
+};
+
 document.querySelector('.check').addEventListener('click', function () {
   const guessNumber = Number(document.querySelector('.guess').value);
   if (currentScore < 1) {
-    document.querySelector('.message').textContent = '💥 You lost the game!';
+    displayMessage('💥 You lost the game!');
     document.querySelector('.score').textContent = 0;
     return;
   }
 
   if (!guessNumber) {
-    document.querySelector('.message').textContent = '⛔️ No number!';
+    displayMessage('⛔️ No number!');
   } else if (guessNumber === secretNumber) {
     if (currentScore > highScore) {
       highScore = currentScore;
       document.querySelector('.highscore').textContent = currentScore;
     }
-    document.querySelector('.message').textContent = '🎉 Correct Number!';
+    displayMessage('🎉 Correct Number!');
     document.querySelector('.number').textContent = secretNumber;
     document.querySelector('body').style.backgroundColor = '#60b347';
     document.querySelector('.number').style.width = '30rem';
     return;
-  } else if (guessNumber > secretNumber) {
+  } else if (guessNumber !== secretNumber) {
     currentScore--;
-    document.querySelector('.message').textContent = '📈 Too High!';
-    document.querySelector('.score').textContent = currentScore;
-  } else if (guessNumber < secretNumber) {
-    currentScore--;
-    document.querySelector('.message').textContent = '📉 Too Low!';
+    document.querySelector('.message').textContent =
+      guessNumber > secretNumber ? '📈 Too High!' : '📉 Too Low!';
     document.querySelector('.score').textContent = currentScore;
   }
 });
@@ -39,8 +40,9 @@ document.querySelector('.again').addEventListener('click', function () {
   document.querySelector('.guess').value = '';
   secretNumber = Math.ceil(Math.random() * 20);
   document.querySelector('.score').textContent = 20;
-  document.querySelector('.message').textContent = 'Star guessing...';
+  displayMessage('Star guessing...');
   document.querySelector('.number').textContent = '?';
   document.querySelector('body').style.backgroundColor = '#333';
   document.querySelector('.number').style.width = '15rem';
+  currentScore = 20;
 });
